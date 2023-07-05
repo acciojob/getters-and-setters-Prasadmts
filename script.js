23ed66c5a87882723592335e0752a59bb7a9ecc0
Prasadmts <124504772+Prasadmts@users.noreply.github.com>
2023-07-05 19:13:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ class Person {
 
 class Student extends Person {
 	study() {
-		console.log(this.name + " is studing");
+		console.log(`${this.name} is studing`);
 	}
 }
 
 class Teacher extends Person {
 	teach() {
-		console.log(this.name +" is teaching");
+		console.log(`${this.name} is teaching`);
 	}
 }
 
